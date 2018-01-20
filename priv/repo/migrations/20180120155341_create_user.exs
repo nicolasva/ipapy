c8ec5d37bab3ae5986cf60e4d7158f225be75571
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2018-01-20 17:05:09 +0100

    add migration user

--- priv/repo/migrations/20180120155341_create_user.exs
+++ priv/repo/migrations/20180120155341_create_user.exs
@@ -2,6 +2,12 @@ defmodule IpapyWeb.Repo.Migrations.CreateUser do
   use Ecto.Migration
 
   def change do
-
+    create table(:users) do
+      add :name, :string
+      add :username, :string, null: false
+      add :encrypted_password, :string
+      timestamps()
+    end
+    create unique_index(:users, [:username])
   end
 end
