4e761080323801543668b13cf225c20a9b399335
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2018-01-24 23:38:46 +0100

    update template email passeword lost

--- web/email.ex
+++ web/email.ex
@@ -1,17 +1,21 @@
 defmodule IpapyWeb.Email do
   use Bamboo.Phoenix, view: IpapyWeb.EmailView
 
-  def send_new_password_temporary_text(email) do
+  def send_new_password_temporary_text(email, password) do
     new_email
     |> to(email)
     |> from("us@example.com")
     |> subject("Welcome!")
     |> text_body("New password")
+    |> put_text_layout({IpapyWeb.LayoutView, "email.text"})
+    |> render("send_password_temporary.text", password: password)
   end
 
-  def send_new_password_temporary_html(email) do
+  def send_new_password_temporary_html(email, password) do
     email
     |> send_new_password_temporary_text()
     |> html_body("New password")
+    |> put_html_layout({IpapyWeb.LayoutView, "email.html"})
+    |> render("send_password_temporary.html", password: password)
   end
 end
