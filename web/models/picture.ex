0cf7e45ea9b48241bbc5ad267692f3078b996e0c
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2018-12-26 23:56:15 +0100

    update picture and refact code

--- web/models/picture.ex
+++ web/models/picture.ex
@@ -20,25 +20,40 @@ defmodule IpapyWeb.Picture do
   def changeset(model, params \\ :invalid) do
     model
     |> cast(params, ~w(title preview_photo picture), [])
-    |> preview_photo_params(model)
+    |> change_preview_photo
+    |> preview_photo_params
     #|> cast(params, @required_fields, @optional_fields)
     |> validate_required([:retirement_home_id])
     |> cast_attachments(params, [:picture])
   end
 
-  def preview_picture(retirement_home_id) do
-    picture = from(p in IpapyWeb.Picture, where: p.retirement_home_id == ^retirement_home_id, where: p.preview_photo == "true", limit: 1) |> IpapyWeb.Repo.one
+  defp change_preview_photo(changeset) do
+    retirement_home_id = get_field(changeset, :retirement_home_id)
+    case count_pictures_by_retirement_home(retirement_home_id) do
+      0 ->
+        force_change(changeset, :preview_photo, "true")
+      _ ->
+        changeset
+    end
   end
 
-  defp preview_photo_params(changeset, model) do
-    try do
-      if model.preview_photo == "true" do
-        retirement_home_id = model.retirement_home_id
-        from(p in IpapyWeb.Picture, where: p.retirement_home_id == ^retirement_home_id, update: [set: [preview_photo: "false"]]) |> IpapyWeb.Repo.update_all([])
-      end
-      changeset
-    rescue
-      x -> changeset
+  def preview_picture(retirement_home_id) do
+    picture = from(p in IpapyWeb.Picture, where: p.retirement_home_id == ^retirement_home_id, where: p.preview_photo == "true", limit: 1) |> IpapyWeb.Repo.one
+    if picture do
+      IpapyWeb.PictureUploader.url({picture.picture, picture}, :thumb)
     end
+  end
+
+  defp preview_photo_params(changeset) do
+    preview_photo = get_field(changeset, :preview_photo)
+    retirement_home_id = get_field(changeset, :retirement_home_id)
+    if preview_photo == "true"  do
+      from(p in IpapyWeb.Picture, where: p.retirement_home_id == ^retirement_home_id, update: [set: [preview_photo: "false"]]) |> IpapyWeb.Repo.update_all([])
+    end
+    changeset
+  end
+
+  defp count_pictures_by_retirement_home(retirement_home_id) do
+    from(p in IpapyWeb.Picture, where: p.retirement_home_id == ^retirement_home_id, select: count(p.id)) |> IpapyWeb.Repo.one
   end
 end
