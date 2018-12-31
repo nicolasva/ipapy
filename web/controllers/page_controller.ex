ea98b98b4fca1698f4427624cf462424d7bb5c2a
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2018-12-31 19:54:59 +0100

    update page_controller with retirement_home

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule IpapyWeb.PageController do
   use IpapyWeb.Web, :controller
 
+  alias IpapyWeb.RetirementHome
+
   def index(conn, _params) do
-    render conn, "index.html"
+    retirement_homes =
+      from(retirement_home in IpapyWeb.RetirementHome, where: retirement_home.rating_director <= 5, where: retirement_home.rating_director >= 3, limit: 10)
+      |> IpapyWeb.Repo.all
+      
+    render conn, "index.html", retirement_homes: retirement_homes
   end
 end
