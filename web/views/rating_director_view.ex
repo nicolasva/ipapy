73f313474029fecbfa7190c3dd5b44937a69dec6
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-01-04 23:06:02 +0100

    update rating_director_view.ex

--- web/views/rating_director_view.ex
+++ web/views/rating_director_view.ex
@@ -2,9 +2,9 @@ defmodule IpapyWeb.RatingDirectorView do
   use IpapyWeb.Web, :view
 
   def render("index.json", %{retirement_home: retirement_home}) do
-    %{id: retirement_home.id, 
-      title: retirement_home.title, 
-      detail: retirement_home.detail, 
+    %{id: retirement_home.id,
+      title: retirement_home.title,
+      detail: retirement_home.detail,
       rating_director: retirement_home.rating_director
      }
   end
