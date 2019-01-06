0b429abc6e24286ceb2b9ae35be0179b013d2a70
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-01-06 23:32:24 +0100

    update appreciation

--- web/controllers/appreciation_controller.ex
+++ web/controllers/appreciation_controller.ex
@@ -6,19 +6,18 @@ defmodule IpapyWeb.AppreciationController do
   def create(conn, %{"appreciation" => appreciation_params, "retirement_home_id" => retirement_home_id}) do
     changeset =
       Repo.get!(IpapyWeb.RetirementHome, retirement_home_id)
-      |> build_assoc(:appreciations)
-      |> conn.assigns.current_user
-      |> build_assoc(:users)
-      |> IpapyWeb.RetirementHome.changeset(appreciation_params)
+      |> build_assoc(:appreciations, user_id: conn.assigns.current_user.id)
+      |> IpapyWeb.Appreciation.changeset(appreciation_params)
       #|> force_change(:user_id, conn.assigns.current_user.id)
 
     case Repo.insert(changeset) do
-      {:ok, _retirement_home} ->
+      {:ok, _appreciation} ->
         conn
         |> put_flash(:info, "Cette appréciation a bien été enregistré.")
         |> redirect(to: retirement_home_path(conn, :show, retirement_home_id))
       {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        conn
+        |> redirect(to: retirement_home_path(conn, :show, retirement_home_id))
     end
   end
 end
--- web/models/appreciation.ex
+++ web/models/appreciation.ex
@@ -4,18 +4,14 @@ defmodule IpapyWeb.Appreciation do
   schema "appreciations" do
     field :title, :string
     field :comment, :string
-    belongs_to :retirement_home_id, IpapyWeb.RetirementHome
-    belongs_to :user_id, IpapyWeb.User
+    belongs_to :retirement_home, IpapyWeb.RetirementHome
+    belongs_to :user, IpapyWeb.User
     timestamps()
   end
 
   def changeset(model, params \\ :invalid) do
     model
     |> cast(params, ~w(title comment), [])
-    |> validate_required([:title, :comment, :retirement_home_id, :user_id])
-  end
-
-  defp set_user(changeset) do
-    force_change(changeset, :user_id, conn.assigns.current_user.id)
+    |> validate_required([:title, :comment])
   end
 end
