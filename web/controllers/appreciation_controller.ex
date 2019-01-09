216f2716f50a860f37e9c51540c7f45cb108c70e
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-01-09 14:32:24 +0100

    update appreciation new

--- web/controllers/appreciation_controller.ex
+++ web/controllers/appreciation_controller.ex
@@ -3,9 +3,22 @@ defmodule IpapyWeb.AppreciationController do
 
   alias IpapyWeb.Appreciation
 
-  def create(conn, %{"appreciation" => appreciation_params, "retirement_home_id" => retirement_home_id}) do
+  def new(conn, %{"retirement_home_id" => retirement_home_id}) do
+    retirement_home = Repo.get!(IpapyWeb.RetirementHome, 11)
+                      |> Repo.preload([[appreciations: :user], :location])
     changeset =
-      Repo.get!(IpapyWeb.RetirementHome, retirement_home_id)
+      retirement_home
+      |> build_assoc(:appreciations)
+      |> IpapyWeb.Appreciation.changeset()
+
+    render(conn, "new.html", retirement_home: retirement_home, changeset: changeset)
+  end
+
+  def create(conn, %{"appreciation" => appreciation_params, "retirement_home_id" => retirement_home_id}) do
+    retirement_home = Repo.get!(IpapyWeb.RetirementHome, retirement_home_id)
+                      |> Repo.preload([[appreciations: :user], :location])
+    changeset =
+      retirement_home 
       |> build_assoc(:appreciations, user_id: conn.assigns.current_user.id)
       |> IpapyWeb.Appreciation.changeset(appreciation_params)
       #|> force_change(:user_id, conn.assigns.current_user.id)
@@ -17,7 +30,10 @@ defmodule IpapyWeb.AppreciationController do
         |> redirect(to: retirement_home_path(conn, :show, retirement_home_id))
       {:error, changeset} ->
         conn
-        |> redirect(to: retirement_home_path(conn, :show, retirement_home_id))
+        #|> redirect(to: retirement_home_path(conn, :show, retirement_home_id))
+        |> render("new.html", changeset: changeset, retirement_home: retirement_home)
+        #conn
+        #|> redirect(to: retirement_home_appreciation_path(conn, :new, retirement_home_id))
     end
   end
 end
